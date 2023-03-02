307b6f8891922d35fa702cc8e862f8897b051e4f
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-02 12:53:02 +0100

    Adds file upload example

--- src/server.js
+++ src/server.js
@@ -2,21 +2,26 @@
 import Express from "express" // NEW IMPORT SYNTAX (We can use it only if we add "type": "module", to package.json)
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
+import { join } from "path"
 import usersRouter from "./api/users/index.js"
 import booksRouter from "./api/books/index.js"
+import filesRouter from "./api/files/index.js"
 import { genericErrorHandler, badRequestHandler, unauthorizedHandler, notfoundHandler } from "./errorsHandlers.js"
 
 const server = Express()
 const port = 3001
+const publicFolderPath = join(process.cwd(), "./public")
 
 // ************************** GLOBAL MIDDLEWARES *********************
 
+server.use(Express.static(publicFolderPath))
 server.use(cors())
 server.use(Express.json()) // If you don't add this line BEFORE the endpoints all request bodies will be UNDEFINED!!!!!!!!!!!!!!!
 
 // ************************** ENDPOINTS ***********************
 server.use("/users", usersRouter)
 server.use("/books", booksRouter)
+server.use("/files", filesRouter)
 
 // ************************* ERROR HANDLERS *******************
 server.use(badRequestHandler) // 400
